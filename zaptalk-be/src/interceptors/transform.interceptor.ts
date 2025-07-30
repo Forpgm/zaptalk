b902e2b8d23840d36c6db64813c8d20d764a589f
Forpgm <phm.giamy@gmail.com>
2025-07-31 03:43:06 +0700

    intercepters

--- zaptalk-be/src/interceptors/transform.interceptor.ts
+++ zaptalk-be/src/interceptors/transform.interceptor.ts
@@ -6,19 +6,32 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Response } from 'express';
 
-export interface Response<T> {
+export interface ApiResponse<T> {
+  statusCode: number;
+  message: string;
   data: T;
 }
 
 @Injectable()
 export class TransformInterceptor<T>
-  implements NestInterceptor<T, Response<T>>
+  implements NestInterceptor<T, ApiResponse<T>>
 {
   intercept(
     context: ExecutionContext,
     next: CallHandler,
-  ): Observable<Response<T>> {
-    return next.handle().pipe(map((data) => ({ data })));
+  ): Observable<ApiResponse<T>> {
+    const ctx = context.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const statusCode = res.statusCode;
+
+    return next.handle().pipe(
+      map((data: T) => ({
+        statusCode,
+        message: 'Success',
+        data,
+      })),
+    );
   }
 }
--- zaptalk-be/src/main.ts
+++ zaptalk-be/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/exception.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 
