d221fb261296ab1712093cf809c95c33316d6a3f
Forpgm <phm.giamy@gmail.com>
2025-08-01 18:03:29 +0700

    refactor check duplicate fields

--- zaptalk-be/src/modules/auth/auth.service.ts
+++ zaptalk-be/src/modules/auth/auth.service.ts
@@ -4,57 +4,43 @@ import { AUTH_MESSAGES } from 'src/constants/messages';
 import { hashPassword } from 'src/utils/bcrypt';
 import { RegisterType } from './schema/register.schema';
 import { ValidationException } from 'src/filters/validation.exception';
+import { TokenService } from './token.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly tokenService: TokenService,
+  ) {}
   async register(payload: RegisterType) {
     const { email, password, first_name, last_name, phone_number, username } =
       payload;
-    const existingUsername = await this.prisma.users.findFirst({
+    const existingUser = await this.prisma.users.findFirst({
       where: {
-        username,
         deleted_at: null,
         deleted_by: null,
+        OR: [{ username }, { email }, { phone_number }],
       },
     });
-    const existingEmail = await this.prisma.users.findFirst({
-      where: {
-        email,
-        deleted_at: null,
-        deleted_by: null,
-      },
-    });
-    const existingPhoneNumber = await this.prisma.users.findFirst({
-      where: {
-        phone_number,
-        deleted_at: null,
-        deleted_by: null,
-      },
-    });
-    if (existingUsername) {
-      throw new ValidationException([
-        {
+    if (existingUser) {
+      const errors: { field: string; message: string }[] = [];
+      if (existingUser.username === username)
+        errors.push({
           field: 'username',
           message: AUTH_MESSAGES.USERNAME_ALREADY_EXISTED,
-        },
-      ]);
-    }
-    if (existingEmail) {
-      throw new ValidationException([
-        {
+        });
+      if (existingUser.email === email)
+        errors.push({
           field: 'email',
           message: AUTH_MESSAGES.EMAIL_ALREADY_EXISTED,
-        },
-      ]);
-    }
-    if (existingPhoneNumber) {
-      throw new ValidationException([
-        {
+        });
+      if (existingUser.phone_number === phone_number)
+        errors.push({
           field: 'phone_number',
           message: AUTH_MESSAGES.PHONE_NUMBER_ALREADY_EXISTED,
-        },
-      ]);
+        });
+
+      throw new ValidationException(errors);
     }
 
     const user = await this.prisma.users.create({
@@ -67,6 +53,26 @@ export class AuthService {
         phone_number,
       },
     });
-    return user;
+
+    const { access_token, refresh_token } =
+      await this.tokenService.signAccessAndRefreshToken(
+        user.id,
+        user.email,
+        user.role,
+        user.username,
+      );
+    return {
+      user: {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        phone_number: user.phone_number,
+        avatar_url: user.avatar_url,
+      },
+      access_token,
+      refresh_token,
+    };
   }
 }
