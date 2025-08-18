8782ec9818dbb63072378150f4010f8cb42f2fe2
Forpgm <phm.giamy@gmail.com>
2025-08-18 14:50:39 +0700

    rejected route for auth login

--- zaptalk-fe/src/routes/login.tsx
+++ zaptalk-fe/src/routes/login.tsx
@@ -1,6 +1,14 @@
+import { path } from "@/constants/path";
 import AuthPage from "@/pages/auth/AuthPage";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
+  beforeLoad: ({ context }) => {
+    if (context.auth.isAuthenticated) {
+      throw redirect({
+        to: path.inbox,
+      });
+    }
+  },
   component: AuthPage,
 });
